8518ec5846c619169df8c15d7e1e18d271de232d
gabbydgab <gamba@gabbydgab.com>
2016-06-15 11:36:27 +0800

    updates

--- module/Application/config/templates.config.php
+++ module/Application/config/templates.config.php
@@ -3,7 +3,11 @@
 return [
     // Zend Framework Configuration
     'view_manager' => [
-        'template_map' => [],
+        'template_map' => [
+            'partial/header'            => __DIR__ . '/../templates/layout/partial/header.phtml',
+            'partial/footer'            => __DIR__ . '/../templates/layout/partial/footer.phtml',
+            'widget/banner/footer'      => __DIR__ . '/../templates/layout/widget/banner/footer.phtml',
+        ],
         'template_path_stack' => [
             'application' => __DIR__ . '/../templates',
         ]
@@ -11,7 +15,11 @@ return [
 
     // Zend Expressive Configuration
     'templates'     => [
-        'map'       => [],
+        'map'       => [
+            'partial/header'            => __DIR__ . '/../templates/layout/partial/header.phtml',
+            'partial/footer'            => __DIR__ . '/../templates/layout/partial/footer.phtml',
+            'widget/banner/footer'      => __DIR__ . '/../templates/layout/widget/banner/footer.phtml',
+        ],
         'paths'     => [
             'application'  => [__DIR__ . '/../templates']
         ]
